ec84f85280bcfcc5755c52de9ba18b466e81e3d5
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-06-19 09:56:17 +0700

    base ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+import LoginPage from './pages/auth/login/LoginPage'
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className='flex max-w-6xl mx-auto'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App

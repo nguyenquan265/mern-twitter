c7ea35dcb3351da67e7b9321fe5ccb34279f10ba
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-06-21 17:35:17 +0700

    deployment 2.1

--- server/src/cron.js
+++ server/src/cron.js
@@ -1,9 +1,9 @@
-import cron from 'cron'
+import { CronJob } from 'cron'
 import https from 'https'
 
 const URL = 'https://mern-twitter-dd71.onrender.com/api/v1/check'
 
-const job = new cron.CronJob('*/14 * * * *', function () {
+const job = new CronJob('*/14 * * * *', function () {
   https
     .get(URL, (res) => {
       if (res.statusCode === 200) {
